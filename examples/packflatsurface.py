85065f449a31091a792943f0aa8a6db6219867fb
Alyssa Travitz <atravitz@umich.edu>
2019-12-19 20:39:20 -0500

    fixing syntax in examples

--- examples/packflatsurface.py
+++ examples/packflatsurface.py
@@ -1,8 +1,9 @@
 from packmol_wrapper import PackmolInput
 
-#Specify dimensions
-packmol_tol    = 1.1
-box_buffer     = 0.
+
+# Specify dimensions
+packmol_tol = 1.1
+box_buffer = 0.
 
 num_polymers = 100
 num_kuhn = 10
@@ -24,38 +25,38 @@ z_max = box_height/2-0.1
 
 inp = PackmolInput(packmol_tol, 'xyz', "{}loop_{}kuhn_{}ly_{}gap.xyz".format(num_polymers, num_kuhn, box_length, box_width), 2*box_buffer)
 
-#Left Loops
+# Left Loops
 left_loops = inp.addStructure('10kuhn.xyz', num_lloops)
 inp.addStructureConstraintInsideBox(x_min, y_min, z_min, x_max, y_max, z_max)
 left_loops.pickAtoms(1)
 left_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.1)
 left_loops.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_min+0.4)
-left_loops.pickAtoms(list(range(2,11)))
+left_loops.pickAtoms(list(range(2, 11)))
 left_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.4)
 left_loops.pickAtoms(11)
 left_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.1)
 left_loops.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_min+0.4)
 
-#Bridges
+# Bridges
 bridges = inp.addStructure('10kuhn_bridge.xyz', num_bridges)
 inp.addStructureConstraintInsideBox(x_min, y_min, z_min, x_max, y_max, z_max)
 bridges.pickAtoms(1)
 bridges.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.1)
 bridges.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_min+0.4)
-bridges.pickAtoms(list(range(2,11)))
+bridges.pickAtoms(list(range(2, 11)))
 bridges.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.4)
 bridges.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_max-0.4)
 bridges.pickAtoms(11)
 bridges.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_max-0.4)
 bridges.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_max-0.1)
 
-#Right Loops
+# Right Loops
 right_loops = inp.addStructure('10kuhn.xyz', num_rloops)
 inp.addStructureConstraintInsideBox(x_min, y_min, z_min, x_max, y_max, z_max)
 right_loops.pickAtoms(1)
 right_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_max-0.4)
 right_loops.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_max-0.1)
-right_loops.pickAtoms(list(range(2,11)))
+right_loops.pickAtoms(list(range(2, 11)))
 right_loops.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_max-0.4)
 right_loops.pickAtoms(11)
 right_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_max-0.4)
