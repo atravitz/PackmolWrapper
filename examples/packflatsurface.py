04df8da8ef01ebdb2414acedb09b09b800d05538
Alyssa Travitz <atravitz@umich.edu>
2019-12-30 11:31:58 -0500

    temporarily including absolute path. added in error checking

--- examples/packflatsurface.py
+++ examples/packflatsurface.py
@@ -26,7 +26,7 @@ z_max = box_height/2-0.1
 inp = PackmolInput(packmol_tol, 'xyz', "{}loop_{}kuhn_{}ly_{}gap.xyz".format(num_polymers, num_kuhn, box_length, box_width), 2*box_buffer)
 
 # Left Loops
-left_loops = inp.addStructure('10kuhn.xyz', num_lloops)
+left_loops = inp.addStructure('molecules/10kuhn.xyz', num_lloops)
 inp.addStructureConstraintInsideBox(x_min, y_min, z_min, x_max, y_max, z_max)
 left_loops.pickAtoms(1)
 left_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.1)
@@ -38,7 +38,7 @@ left_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.1)
 left_loops.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_min+0.4)
 
 # Bridges
-bridges = inp.addStructure('10kuhn_bridge.xyz', num_bridges)
+bridges = inp.addStructure('molecules/10kuhn_bridge.xyz', num_bridges)
 inp.addStructureConstraintInsideBox(x_min, y_min, z_min, x_max, y_max, z_max)
 bridges.pickAtoms(1)
 bridges.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_min+0.1)
@@ -51,7 +51,7 @@ bridges.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_max-0.4)
 bridges.addAtomConstraintBelowPlane(1.0, 0.0, 0.0, x_max-0.1)
 
 # Right Loops
-right_loops = inp.addStructure('10kuhn.xyz', num_rloops)
+right_loops = inp.addStructure('molecules/10kuhn.xyz', num_rloops)
 inp.addStructureConstraintInsideBox(x_min, y_min, z_min, x_max, y_max, z_max)
 right_loops.pickAtoms(1)
 right_loops.addAtomConstraintOverPlane(1.0, 0.0, 0.0, x_max-0.4)
